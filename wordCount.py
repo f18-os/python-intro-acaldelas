fd6a73e293fe38acdcf4529753ba06a56d77e45d
acaldelas <acaldelas@miners.utep.edu>
2018-09-05 09:46:45 -0600

    Add files via upload

--- wordCount.py
+++ wordCount.py
@@ -9,14 +9,37 @@
 #
 #! /usr/bin/env python3
 #Import cmd arguments, checking if file exists, regular expressions, and executing
-import sys, os, re, subprocess
+import sys, os, re
 
 #set input and output files
 if len(sys.argv) is not 3:
     print("Correct usage: wordCount.py <input text file> <output file>")
     exit()
-textFname = sys.argv[1]
+inputFname = sys.argv[1]
 outputFname = sys.argv[2]
+if not os.path.exists(inputFname):
+    print(f"File {inputFname} does not exist")
+    exit()
 
 
+#Open file
+inputread = open(inputFname, 'r')
+
+#Dictionary
+diction = {}
+
+for line in inputread:
+    line = line.strip().lower()
+    for word in line.split():
+        word = re.sub('[a-zA-Z]','',line)
+        if word not in diction:
+            diction[word] = 1
+        else:
+            diction += 1
+inputread.close()
+
+with open(outputFname, 'w') as file:
+    for k,v in diction.items():
+        w = str(k)+'\b'+str(v)+'\n'
+        file.write(w)
 
